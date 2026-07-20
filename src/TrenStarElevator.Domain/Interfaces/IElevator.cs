d854c483f9bb63b62d8eeb13108af2e235d2aa0a
mandlamt <mandlamt@users.noreply.github.com>
2026-07-20 08:19:39 +0200

    Add comment to IElevator interface for clarity
    Added comment to clarify the purpose of the IElevator interface.

--- src/TrenStarElevator.Domain/Interfaces/IElevator.cs
+++ src/TrenStarElevator.Domain/Interfaces/IElevator.cs
@@ -1,5 +1,5 @@
 namespace TrenStarElevator.Domain.Interfaces;
-
+/*SOLID OCP  IElevator interface allows adding new elevator types*/
 public interface IElevator : 
     IElevatorStatus, 
     IElevatorMovement, 
