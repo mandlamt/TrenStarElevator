9a9900a4588903a4908746ca881c69b7ff4731c8
mandlamt <mandlamt@users.noreply.github.com>
2026-07-20 08:20:30 +0200

    Enhance comment for IElevator interface
    Expanded comment to clarify the purpose of the IElevator interface and its extensibility.

--- src/TrenStarElevator.Domain/Interfaces/IElevator.cs
+++ src/TrenStarElevator.Domain/Interfaces/IElevator.cs
@@ -1,5 +1,7 @@
 namespace TrenStarElevator.Domain.Interfaces;
-/*SOLID OCP  IElevator interface allows adding new elevator types*/
+/*SOLID OCP  IElevator interface allows adding new elevator types
+Can add FreightElevator, HighSpeedElevator without changing core code
+*/
 public interface IElevator : 
     IElevatorStatus, 
     IElevatorMovement, 
